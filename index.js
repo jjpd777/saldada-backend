fdf9e712f3e405d2e2a14a43e7a03bc0d5a6cbf4
juan <jpalacio@axletravel.com>
2021-11-07 11:53:19 -0500

    yo

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const APP_PORT = process.env.APP_PORT || 8000;
 const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
 const PLAID_SECRET = process.env.PLAID_SECRET;
 const PLAID_ENV = process.env.PLAID_ENV || 'sandbox';
+console.log(APP_PORT);
+console.log("PORT NUMB")
 
 // PLAID_PRODUCTS is a comma-separated list of products to use when initializing
 // Link. Note that this list must contain 'assets' in order for the app to be
