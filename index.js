cacfa2a3fb1c1c437395aae9eca7806b5a5d7be0
juan <jpalacio@axletravel.com>
2021-11-11 18:02:04 -0500

    deploying to heroku

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.post('/api/info', function (request, response, next) {
   });
 });
 
-app.get('/t', function(request,response){
+app.get('/api/t', function(request,response){
   console.log("\n TESTING STANDARD GET ENDPOINT\n")
   try{
     response.json({successful7: true});
